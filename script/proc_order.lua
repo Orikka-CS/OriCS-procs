ed2af5d8fe64a6ca3348cc1e69e0058c338551d4
slyni <slyni@naver.com>
2025-09-18 11:29:27 +0900

    Update proc_order.lua

--- script/proc_order.lua
+++ script/proc_order.lua
@@ -1,15 +1,13 @@
---[[
-CUSTOMTYPE_ORDER	=0x20
-TYPE_ORDER		=0x10000000
-REASON_ORDER		=0x40000000
-SUMMON_TYPE_ORDER	=0x40005000
-SUMMON_TYPE_ORDER_L	=0x40005010
-SUMMON_TYPE_ORDER_R	=0x40005020
+CUSTOMTYPE_ORDER=0x20
+TYPE_ORDER=0x10000000
+REASON_ORDER=0x40000000
+SUMMON_TYPE_ORDER=0x40005000
+SUMMON_TYPE_ORDER_L=0x40005010
+SUMMON_TYPE_ORDER_R=0x40005020
 
-EFFECT_CANNOT_BE_ORDER_MATERIAL	=700
-EFFECT_EXTRA_ORDER_MATERIAL	=701
-EFFECT_MUST_BE_OMATERIAL	=702
---]]
+EFFECT_CANNOT_BE_ORDER_MATERIAL=700
+EFFECT_EXTRA_ORDER_MATERIAL=701
+EFFECT_MUST_BE_OMATERIAL=702
 
 function aux.ordlimit(e,se,sp,st)
 	return st&SUMMON_TYPE_ORDER==SUMMON_TYPE_ORDER
@@ -36,18 +34,18 @@ end
 function Card.IsOrderMaterialCount(c,count,greater_or_equal_or_less)
 	if greater_or_equal_or_less=="Greater" then
 		while count<=5 do
-			if c:IsHasEffect(99000300+count) then
+			if c:IsHasEffect(99000370+count) then
 				return true
 			end
 			count=count+1
 		end
 	elseif greater_or_equal_or_less=="Equal" then
-		if c:IsHasEffect(99000300+count) then
+		if c:IsHasEffect(99000370+count) then
 			return true
 		end
 	elseif greater_or_equal_or_less=="Less" then
 		while count>=1 do
-			if c:IsHasEffect(99000300+count) then
+			if c:IsHasEffect(99000370+count) then
 				return true
 			end
 			count=count-1
@@ -58,32 +56,22 @@ function Card.IsOrderMaterialCount(c,count,greater_or_equal_or_less)
 end
 
 function Duel.OrderSummon(p,c)
-	if not (c:IsCustomType(CUSTOMTYPE_ORDER) or c:IsType(TYPE_ORDER)) then
+	if not c:IsType(TYPE_ORDER) then
 		return false
 	else
 		Duel.SpecialSummonRule(p,c,c:GetSummonType())
 	end
 end
 
---커스텀타입 오더
-
-function Auxiliary.AddCustomTypeOrder(c)
-	local mt=getmetatable(c)
-	mt.CardType_Order=true
-	mt.custom_type=CUSTOMTYPE_ORDER
-end
-
 --오더 소환 유틸리티
 
 function Auxiliary.AddOrderProcedure(c,dir,gf,...)
 	local f={...}
-	local mt=getmetatable(c)
-	Auxiliary.AddCustomTypeOrder(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(9900)
+	e1:SetDescription(aux.Stringid(99000300,4))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetRange(LOCATION_EXTRA)
 	e1:SetCondition(Auxiliary.OrderCondition(gf,table.unpack(f)))
 	e1:SetTarget(Auxiliary.OrderTarget(gf,table.unpack(f)))
@@ -99,7 +87,7 @@ function Auxiliary.AddOrderProcedure(c,dir,gf,...)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetCode(99000300+#f)
+	e2:SetCode(99000370+#f)
 	c:RegisterEffect(e2)
 end
 function Auxiliary.OrderConditionFilter(c,ord)
@@ -110,8 +98,8 @@ function Auxiliary.OrderCheckGoal(sg,tp,oc,gf,...)
 	return Duel.GetLocationCountFromEx(tp,tp,sg,oc)>0 and (not gf or gf(sg))
 		and sg:IsExists(Auxiliary.OrderStartFilter,1,nil,sg,tp,table.unpack(f))
 end
-function Auxiliary.OrderCheckFilter(c,sg,f)
-	return c and sg:IsContains(c) and (not f or f(c))
+function Auxiliary.OrderCheckFilter(c,sg,f,oc,tp)
+	return c and sg:IsContains(c) and (not f or f(c,oc,SUMMON_TYPE_ORDER,tp))
 end
 function Auxiliary.OrderStartFilter(c,sg,tp,...)
 	local f={...}
@@ -122,8 +110,8 @@ function Auxiliary.OrderStartFilter(c,sg,tp,...)
 	for i=0,#f-1 do
 		local tc=Duel.GetFieldCard(tp,LOCATION_MZONE,seq+i)
 		local ec=(seq+i)&1>0 and Duel.GetFieldCard(tp,LOCATION_MZONE,(seq+i+9)/2)
-		if not (aux.OrderCheckFilter(tc,sg,f[i+1])
-			or aux.OrderCheckFilter(ec,sg,f[i+1])) then
+		if not (aux.OrderCheckFilter(tc,sg,f[i+1],c,tp)
+			or aux.OrderCheckFilter(ec,sg,f[i+1],c,tp)) then
 		return false
 		end
 	end
@@ -149,7 +137,7 @@ function Auxiliary.OrderTarget(gf,...)
 		local mg=Duel.GetMatchingGroup(Auxiliary.OrderConditionFilter,tp,LOCATION_MZONE,0,nil,c)
 		local fg=Auxiliary.GetMustMaterialGroup(tp,EFFECT_MUST_BE_OMATERIAL)
 		Duel.SetSelectedCard(fg)
-		Duel.Hint(HINT_SELECTMSG,tp,9901)
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(99000300,5))
 		local cancel=Duel.IsSummonCancelable()
 		local sg=mg:SelectSubGroup(tp,Auxiliary.OrderCheckGoal,cancel,#f,#f,tp,c,gf,table.unpack(f))
 		if sg then
@@ -177,51 +165,52 @@ function Auxiliary.OrderOperation(gf,...)
 end
 
 --융합 타입 삭제
-local type=Card.GetType
-Card.GetType=function(c)
+
+	local type=Card.GetType
+	Card.GetType=function(c)
 	if c.CardType_Order then
 		return bit.bor(type(c),TYPE_FUSION)-TYPE_FUSION
 	end
 	return type(c)
 end
 --
-local otype=Card.GetOriginalType
-Card.GetOriginalType=function(c)
+	local otype=Card.GetOriginalType
+	Card.GetOriginalType=function(c)
 	if c.CardType_Order then
 		return bit.bor(otype(c),TYPE_FUSION)-TYPE_FUSION
 	end
 	return otype(c)
 end
 --
-local ftype=Card.GetFusionType
-Card.GetFusionType=function(c)
+	local ftype=Card.GetFusionType
+	Card.GetFusionType=function(c)
 	if c.CardType_Order then
 		return bit.bor(ftype(c),TYPE_FUSION)-TYPE_FUSION
 	end
 	return ftype(c)
 end
 --
-local ptype=Card.GetPreviousTypeOnField
-Card.GetPreviousTypeOnField=function(c)
+	local ptype=Card.GetPreviousTypeOnField
+	Card.GetPreviousTypeOnField=function(c)
 	if c.CardType_Order then
 		return bit.bor(ptype(c),TYPE_FUSION)-TYPE_FUSION
 	end
 	return ptype(c)
 end
 --
-local itype=Card.IsType
-Card.IsType=function(c,t)
+	local itype=Card.IsType
+	Card.IsType=function(c,t,...)
 	if c.CardType_Order then
 		if t==TYPE_FUSION then
 			return false
 		end
-		return itype(c,bit.bor(t,TYPE_FUSION)-TYPE_FUSION)
+		return itype(c,bit.bor(t,TYPE_FUSION)-TYPE_FUSION,...)
 	end
-	return itype(c,t)
+	return itype(c,t,...)
 end
 --
-local iftype=Card.IsFusionType
-Card.IsFusionType=function(c,t)
+	local iftype=Card.IsFusionType
+	Card.IsFusionType=function(c,t)
 	if c.CardType_Order then
 		if t==TYPE_FUSION then
 			return false
